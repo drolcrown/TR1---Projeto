73eabf30d9ef2f6ed03db323bc6d731612c21600
Luigi <luigiminardim@gmail.com>
2019-06-10 20:44:24 -0300

    Domínios feitos

--- include/dominios.hpp
+++ include/dominios.hpp
@@ -1,1 +1,14 @@
 #pragma once
+
+#include <vector>
+
+/** @brief Bit é o tipo de dado que pode ser 0 ou 1. */
+typedef bool Bit;
+
+/** @brief Quadro é o tipo de dado que representa um vetor de Bits na camada de
+ *  Enlace. */
+class Quadro : public std::vector<Bit>{};
+
+/** @brief FluxoDeBits pe o tipo de dado que representa um vetor de Bits na
+ * camada física. */
+class FluxoDeBits : public std::vector<Bit>{};
