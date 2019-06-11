ac0deb44803ea0eea5aef392f5be0ce931ef1fe8
Luigi <luigiminardim@gmail.com>
2019-06-10 22:20:25 -0300

    classes Aplicacao adicionadas

--- include/aplicacao.hpp
+++ include/aplicacao.hpp
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "interfaces_comunicacao.hpp"
+
+class AplicacaoTransmissora {
+ public:
+  void run();
+  void configCamadaAplicacaoTransmissora(ICamadaAplicacaoTransmissora*);
+ private:
+  Mensagem criarMensagem();
+  ICamadaAplicacaoTransmissora* camada_aplicacao = nullptr;
+};
+
+class AplicacaoReceptora {
+ public:
+  void receber(const Mensagem&);
+};
