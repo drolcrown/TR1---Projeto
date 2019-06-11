4dbf095c4fb85cf74fa94a69ee5dc9d57e8ec2d5
Luigi <luigiminardim@gmail.com>
2019-06-10 21:59:32 -0300

    Classes do sistema de comunicação adicionadas.

--- include/sistema_comunicacao.hpp
+++ include/sistema_comunicacao.hpp
@@ -6,3 +6,36 @@
 #include "meio_fisico.hpp"
 #include "camada_fisica.hpp"
 #include "codificador_de_bits.hpp"
+
+class Transmissor {
+ private:
+  AplicacaoTransmissora aplicacao;
+  CamadaAplicacaoTransmissora camada_aplicacao;
+  CamadaEnlaceTransmissora camada_enlace;
+  CamadaFisicaTransmissora camada_fisica;
+  MeioFisico* meio_fisico = nullptr;
+ public:
+  void run();
+  void configMeioFisico(MeioFisico*);
+  void configCodificadorDeBits(ICodificadorDeBits*);
+};
+
+class Receptor {
+ private:
+  AplicacaoReceptora aplicacao;
+  CamadaAplicacaoReceptora camada_aplicacao;
+  CamadaEnlaceReceptora camada_enlace;
+  CamadaFisicaReceptora camada_fisica;
+ public:
+  void configCodificadorDeBits(ICodificadorDeBits*);
+};
+
+class SistemaDeComunicacao {
+ public:
+  void run();
+ private:
+  Transmissor transmissor;
+  Receptor receptor;
+  MeioFisico meio_fisico;
+};
+
