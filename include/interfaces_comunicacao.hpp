b0786802e258f304ea06ff08f13d3e281138a294
Luigi <luigiminardim@gmail.com>
2019-06-10 21:30:01 -0300

    interfaces de comunicação feitas.

--- include/interfaces_comunicacao.hpp
+++ include/interfaces_comunicacao.hpp
@@ -1,3 +1,62 @@
 #pragma once
 
 #include "dominios.hpp"
+#include "codificador_de_bits.hpp"
+#include "meio_fisico"
+
+class AplicacaoReceptora; // Classe é declarada no arquivo aplicacao.hpp
+                          // porém o arquivo inclui este. 
+
+class ICamadaAplicacaoReceptora {
+ public:
+  virtual void receber(const Quadro&) = 0;
+  void configAplicacaoReceptora(AplicacaoReceptora*);
+ protected:
+  virtual Mensagem convQuadroPorMensagem(const Quadro&) = 0;
+  AplicacaoReceptora* aplicacao = nullptr;
+};
+
+class ICamadaEnlaceReceptora {
+ public:
+  void virtual receber(const Quadro&) = 0;
+  void configCamadaAplicacaoReceptora(ICamadaAplicacaoReceptora*);
+ private:
+  ICamadaAplicacaoReceptora* camada_aplicacao = nullptr;
+};
+
+class ICamadaFisicaReceptora {
+ public:
+  virtual void receber(const FluxoDeBits&) = 0;
+  void configCamadaEnlaceReceptora(ICamadaEnlaceReceptora*);
+  void configCodificador(ICodificadorDeBits*);
+ private:
+  ICamadaEnlaceReceptora* camada_enlace = nullptr;
+  ICodificadorDeBits* codificador = nullptr;
+};
+
+class ICamadaFisicaTransmissora {
+ public:
+  virtual void transmitir(const Quadro&) = 0;
+  void configMeioFisico(MeioFisico*);
+  void configCodificador(ICodificadorDeBits*); 
+ protected:
+  MeioFisico* meio_fisico = nullptr;
+  ICodificadorDeBits* codificador = nullptr;
+};
+
+class ICamadaEnlaceTransmissora {
+ public:
+  virtual void transmitir(const Quadro&) = 0;
+  void configCamadaFisicaTransmissora(ICamadaFisicaTransmissora*);
+ protected:
+  ICamadaFisicaTransmissora* camada_fisica = nullptr;
+};
+
+class ICamadaAplicacaoTransmissora {
+ public:
+  virtual void transmitir(const Mensagem&) = 0;
+  void configCamadaEnlaceTransmissora(ICamadaEnlaceTransmissora*); 
+ protected:
+  virtual Quadro convMensagemPorQuadro(const Mensagem&) = 0;
+  ICamadaEnlaceTransmissora* camada_enlace = nullptr;
+};
