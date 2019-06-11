5fbf44b6e3a45e8851d2a0cf60ded6ad5bd3632f
Luigi <luigiminardim@gmail.com>
2019-06-11 04:21:07 -0300

    Assinatura das camadas de rede feitas.

--- include/camada_enlace.hpp
+++ include/camada_enlace.hpp
@@ -1,1 +1,9 @@
 #pragma once
+
+class CamadaEnlaceTransmissora {
+
+};
+
+class CamadaEnlaceReceptora {
+
+};
--- include/camada_fisica.hpp
+++ include/camada_fisica.hpp
@@ -1,1 +1,9 @@
 #pragma once
+
+class CamadaFisicaTransmissora {
+
+};
+
+class CamadaFisicaReceptora {
+
+};
