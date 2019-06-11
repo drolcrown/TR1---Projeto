273d355fd48acef4982ff2a30f74c47c9d61a814
Luigi <luigiminardim@gmail.com>
2019-06-10 22:00:27 -0300

    classe ICodificadorDeBits adicionada.

--- include/codificador_de_bits.hpp
+++ include/codificador_de_bits.hpp
@@ -1,3 +1,9 @@
 #pragma once
 
 #include "dominios.hpp"
+
+class ICodificadorDeBits {
+ public:
+  virtual FluxoDeBits codificar(const Quadro&) = 0;
+  virtual Quadro decodificar(const FluxoDeBits) = 0;
+};
