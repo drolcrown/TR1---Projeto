7acd3fe1ac3fcfc19108f391274e9f5ed677b4e5
luigiminardim <luigiminardim@gmail.com>
2019-06-17 20:45:50 -0300

    CodificadorManchester agora implementado no arquivo correto.

--- source/codificador_de_bits.cpp
+++ source/codificador_de_bits.cpp
@@ -38,3 +38,36 @@ Quadro CodificadorManchesterDiferencial::decodificar(const FluxoDeBits& fluxo_de
   }
   return quadro;
 }
+
+// CodificadorManchester //////////////////////////////////////////////////////
+
+FluxoDeBits CodificadorManchester::codificar(const Quadro &quadro)
+{
+
+  // 0 = 01 e 1 = 10 ,vetor tem o dobro do tamanho
+
+  int i, aux = 0;
+  FluxoDeBits fluxo;
+  fluxo.reserve(2 * quadro.size());
+
+  for (int i = 0; i < quadro.size(); ++i)
+  {
+
+    if (quadro[i] == 0)
+    {
+      fluxo.push_back(0);
+      fluxo.push_back(1); //se é 0 manda 01
+    }
+
+    else if (quadro[i] == 1)
+    {
+      fluxo.push_back(1);
+      fluxo.push_back(0); //se é 1 manda 10
+    }
+
+    else
+    {
+      std::cout << "Algo de errado nao esta certo\n" // era pra ser só 0 e 1, algo tá muito errado
+    }
+  }
+};
