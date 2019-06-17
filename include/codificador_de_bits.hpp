8595fc816dab299592f3ebdd9bec027ddec4a869
luigiminardim <luigiminardim@gmail.com>
2019-06-16 21:11:52 -0300

    codificador de manchester feito e testado

--- include/codificador_de_bits.hpp
+++ include/codificador_de_bits.hpp
@@ -5,5 +5,11 @@
 class ICodificadorDeBits {
  public:
   virtual FluxoDeBits codificar(const Quadro&) = 0;
-  virtual Quadro decodificar(const FluxoDeBits) = 0;
+  virtual Quadro decodificar(const FluxoDeBits&) = 0;
+};
+
+class CodificadorManchesterDiferencial : ICodificadorDeBits {
+ public:
+  FluxoDeBits codificar(const Quadro&) override;
+  Quadro decodificar(const FluxoDeBits&) override { };
 };
