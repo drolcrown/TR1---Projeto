d05a451af84a4f708280a2dccf12f1eb028c9a93
luigiminardim <luigiminardim@gmail.com>
2019-06-18 14:59:40 -0300

    Print da camada de enlace feito

--- source/camada_enlace.cpp
+++ source/camada_enlace.cpp
@@ -1,9 +1,11 @@
 #include "camada_enlace.hpp"
 
 void CamadaEnlaceTransmissora::transmitir(const Quadro& quadro) {
+  std::cout << "Camada Enlace transmitiu: " << quadro << '\n';
   this->camada_fisica->transmitir(quadro);
 }
 
 void CamadaEnlaceReceptora::receber(const Quadro& quadro) {
+  std::cout << "Camada Enlace recebeu: " << quadro << '\n';
   this->camada_aplicacao->receber(quadro);
 }
