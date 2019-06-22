fe58e5521c36fdbcf93063d5d364d01c59595bac
luigiminardim <luigiminardim@gmail.com>
2019-06-22 20:26:51 -0300

    Novo UML e parte do projeto

--- source/main.cpp
+++ source/main.cpp
@@ -1,36 +1,10 @@
 #include "sistema_comunicacao.hpp"
 
+
 int main() {
-    // Instanciar sistema com codificador binario sem taxa de erros no meio fisico
-    // SistemaDeComunicacao sistema_de_comunicacao = SistemaDeComunicacao();
-    
-    //Instanciar sistema passando codificador e taxa de erros como parametros
-    // CodificadorBinario codificador;
-    // double taxaDeErro = 20.0;
-    // SistemaDeComunicacao sistema_de_comunicacao = SistemaDeComunicacao(codificador, taxaDeErro /** taxa de erro do Meio Fisico**/);
-
-
-    //Lista de tuplas contendo todos os meio fisicos e todos os codificadores
-    std::vector<std::tuple<ICodificadorDeBits*, std::string>> codificadores = {
-        {new CodificadorBinario(), "Codificador Binario"}, 
-        {new CodificadorManchester(), "Codificador Manchester"}, 
-        {new CodificadorManchesterDiferencial(), "Codificador Manchester Diferencial"}
-    };
-    std::vector<std::tuple<MeioFisico*, std::string>> meios = {
-        {new FibraOtica(), "Fibra Otica"}, {new RedeSemFio(), "Rede Sem Fio"}, {new FioTrancado(), "Fio Trançado"}
-    };
-
-    for (int i = 0; i < codificadores.size() ; i++){
-        for (int j = 0; j < meios.size() ; j++){
-            //Instanciar sistema passando codificador e tipo de meio fisico como parametros
-            SistemaDeComunicacao sistema_de_comunicacao = SistemaDeComunicacao(*std::get<0>(codificadores[i]), *std::get<0>(meios[j]) /** taxa de erro do Meio Fisico**/);    
-            std::cout  << esc::color::foreground::BRIGHT_YELLOW 
-            << "Codificador: " << std::get<1>(codificadores[i]) << "\nMeio Fisico: " << std::get<1>(meios[j])
-            << esc::RESET << std::endl;
-            sistema_de_comunicacao.run();
-        }
-    }
-    
-    return 0;
+  ICodificadorDeBits* codificador = new CodificadorBinario;
+  double taxa_de_erros = 0.1;
+  SistemaDeComunicacao sistema_de_comunicacao(codificador, taxa_de_erros);
+  sistema_de_comunicacao.run();
 }
 
