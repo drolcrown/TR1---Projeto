33e185e7e10b948c868cd3e4570e00393c2afa9c
luigiminardim <luigiminardim@gmail.com>
2019-06-25 00:02:21 -0300

    main consertada

--- source/main.cpp
+++ source/main.cpp
@@ -2,10 +2,13 @@
 
 
 int main() {
-  ICodificadorDeBits* codificador = new CodificadorBinario;
+  ICodificadorDeBits* codificador = new CodificadorManchester;
   IControladorDeErro* controlador_de_erro = new ControladorBitParidadePar;
-  double taxa_de_erros = 0.1;
+  int taxa_de_erros = 25; // Porcentagem
   SistemaDeComunicacao sistema_de_comunicacao(codificador, controlador_de_erro, taxa_de_erros);
   sistema_de_comunicacao.run();
+  delete codificador;
+  delete controlador_de_erro;
+  return 0;
 }
 
