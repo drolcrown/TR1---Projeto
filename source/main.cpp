a7e02ab5224246d09685bd89a9b735550f3fd5cd
luigiminardim <luigiminardim@gmail.com>
2019-06-25 14:50:24 -0300

    camada de enlace e main adaptadas

--- source/main.cpp
+++ source/main.cpp
@@ -2,9 +2,16 @@
 
 
 int main() {
-  ICodificadorDeBits* codificador = new CodificadorManchester;
-  IControladorDeErro* controlador_de_erro = new ControladorBitParidadePar;
-  int taxa_de_erros = 25; // Porcentagem
+  ICodificadorDeBits* codificador = 
+    //new CodificadorBinario
+    new CodificadorManchester
+    //new CodificadorManchesterDiferencial
+  ;
+  IControladorDeErro* controlador_de_erro = 
+    //new ControladorBitParidadePar
+    new ControladorBitParidadeImpar
+  ;
+  int taxa_de_erros = 1; // Porcentagem
   SistemaDeComunicacao sistema_de_comunicacao(codificador, controlador_de_erro, taxa_de_erros);
   sistema_de_comunicacao.run();
   delete codificador;
