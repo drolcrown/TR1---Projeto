c849a88536a4e4d0c581a7c425514fb9627c4876
luigiminardim <luigiminardim@gmail.com>
2019-06-26 20:14:05 -0300

    shift left e shift right agora são desnecessários

--- include/controlador_de_erro.hpp
+++ include/controlador_de_erro.hpp
@@ -35,15 +35,6 @@ class ControladorCodigoDeHamming : public IControladorDeErro {
   Quadro adicionarControle(const Quadro&) override;
   Quadro controlarErros(const Quadro& quadro) override;
  private:
-  /** @brief  Desloca o quadro para a esquerda, tirando seu primeiro termo. 
-   * @param quadro  Quadro a ser deslocado.
-   * @return Quadro  Quadro com bits deslocados. (tail quadro) */
-  Quadro shiftLeft(const Quadro& quadro);
-
-  /**  @brief  Deslocar o quadro para direita
-   * @param quadro  Quadro a ser deslocado a direita.
-   * @return Quadro  Quadro com um 0 a mais na esquerda. ([0] ++ quadro) */
-  Quadro shiftRight(const Quadro& quadro);
 
   /** @brief mover os bits de mensagem para as posições corretas pela
    * codificação de Hamming, deixando os índices potencias de 2 com
@@ -61,9 +52,22 @@ class ControladorCodigoDeHamming : public IControladorDeErro {
    * @return Bit */
   Bit bitParidade(int potencia2, const Quadro&);
 
-  Quadro removerBitsParidade(const Quadro&);
+  /** @brief  Retorna um quadro sem a codificação de  no tamanho original.
+   * @param quadro  Quadro com codificação de hamming.
+   * @return Quadro  Quadro sem a codificação de hamming. */
+  Quadro removerBitsParidade(const Quadro& quadro);
 
-  std::vector<int> indicesDosBitsParidadeDiferentes(const Quadro&, const Quadro&);
+  /** @brief Compara os bits de paridades das codificações de hamming.
+   * @return std::vector<int>  Vetor com os índices dos bits de paridade que
+   * não foram iguais. 
+   * @pre  a.size() == b.size() */
+  std::vector<int> indicesDosBitsParidadeDiferentes(const Quadro& a, const Quadro& b);
 
+  /** @brief  A partir de um quadro sem codificação de hamming e de um vetor
+   *   que indica quais bits de paridade estão incorretos, corrige o quadro.
+   * @param quadro  Quadro sem a codificação de hamming.
+   * @param paridades_divergentes  Vetor que mostra os bits de paridade que
+   *  divergiram na codificação de hamming teorica e recebida do quadro.
+   * @return Quadro  Quadro com bits corrigidos. */
   Quadro corrigirQuadro(const Quadro &quadro, const std::vector<int>& paridades_divergentes);
 };
