2ff56632698bc1533215da502561827a77a6ac68
luigiminardim <luigiminardim@gmail.com>
2019-06-26 23:50:25 -0300

    cabeçalho

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,12 @@
+/** @file main.cpp
+ * @authors
+ *   Luigi Minardi Ferreira Maia 17/0017141
+ *   Maria Emília de Castro Oliveira Peres 17/0018199
+ *   Jefferson Augusto Nunes Damasceno 16/0126509
+ *   Ivan Bastos Lancellotti 16/0009057
+ *   Rafael Oliveira de Souza. 15/0081537 
+ * @date 2019-06-26 */
+
 #include "sistema_comunicacao.hpp"
 
 
